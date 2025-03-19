498fe44447e3651ade8d3d0a7cfac6e4acfe8e45
JamesJr0 <jamesjr4690@gmail.com>
2025-03-19 13:57:17 +0530

    Update Mongoclient.py

--- Mongoclient.py
+++ Mongoclient.py
@@ -3,3 +3,5 @@ from creds import cred  # Assuming this contains your MongoDB URL
 
 client = MongoClient(cred.DB_URL, connect=False)  # Add connect=False here
 db = client['your_database_name']  # Replace 'your_database_name' with the actual DB name
+
+client = MongoClient(cred.DB_URL, connect=False)
